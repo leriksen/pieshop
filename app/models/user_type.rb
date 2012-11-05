17b6c564cfa584b1f6771037a99444c65ec64231
leriksen <leif.eriksen.au@gmail.com>
2012-11-06 07:29:27 +1100

    cleanup post merge

--- app/models/user_type.rb
+++ app/models/user_type.rb
@@ -2,9 +2,5 @@ class UserType < ActiveRecord::Base
   attr_accessible :level, :typename
 
   validates_uniqueness_of :typename
-<<<<<<< HEAD
   validates_presence_of [:typename, :level]
-=======
-  validates_presence_of :level
->>>>>>> master
 end
