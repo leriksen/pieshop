21fd4ea91e557e2c1027bb26478d98881af94372
leriksen <leif.eriksen.au@gmail.com>
2012-11-06 07:33:45 +1100

    cleanup post merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20121102202821) do
-=======
-ActiveRecord::Schema.define(:version => 20121105110008) do
->>>>>>> master
 
   create_table "user_types", :force => true do |t|
     t.string   "typename"
