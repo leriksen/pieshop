54f6e23a170c01b2d7ca8424e7121737d5680180
leriksen <leif.eriksen.au@gmail.com>
2012-11-30 22:56:39 +1100

    sidebar render via the 'cells' gem, db seeding for initial state

--- app/cells/sidebar_cell.rb
+++ app/cells/sidebar_cell.rb
@@ -1,11 +1,22 @@
 class SidebarCell < Cell::Rails
 
-  def show
+  def show(args)
     @role_items = []
-    (1..5).each do |n|
-      @role_items << view_context.link_to( "role#{n}", user_types_path)
-    end
+    build_nav_from_role @role_items, args[:user]
     render
   end
 
+private
+  def build_nav_from_role(collection, role)
+    return unless role
+    case role.user_type.typename
+    when "admin"
+      add_link collection, "User Types",  user_types_path
+      add_link collection, "Users",  users_path
+    end
+  end
+
+  def add_link(collection, text,  path)
+    collection << view_context.link_to(text, path)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ut = UserType.find_or_create_by_typename(typename: :admin, level: 99)
+User.find_or_create_by_email(email: "12", password: "12", password_confirmation: "12", user_type_id: ut.id)
